eeb95d94c293ab44aa51f5bc04dd98e14faccc90
Victor Nicollet <victor@nicollet.net>
2013-03-07 23:20:55 +0100

    B. Design interface for beat queue

---
+++ ocaml/mBeat.mli
@@ -0,0 +1,17 @@
+(* pacemkr © 2013 Victor Nicollet *)
+
+(* This module is a "heartbeat queue". Heartbeats are added to this
+   queue by the web server, and then polled and processed by the
+   asynchronous bot servers. *)
+
+type t = {
+  id       : IBeat.t ;
+  account  : IAccount.t ;
+  received : float ;
+  payload  : Api.Post.t list ;
+}
+
+val push : account:IAccount.t -> Api.Post.t -> (#O.ctx,unit) Ohm.Run.t
+
+val pop : unit -> (#O.ctx, t option) Ohm.Run.t
+
