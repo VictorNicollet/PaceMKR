3f4a4a3f113b93b655a4ce0a79b60a6e8d187bf5
Victor Nicollet <victor@nicollet.net>
2013-03-08 00:28:51 +0100

    C. Loosen API requirements

--- ocaml/cBeat.ml
+++ ocaml/cBeat.ml
@@ -14,8 +14,22 @@ let () = Url.def_beat begin fun req res ->
     "status", Json.String "error" ; 
     "reason", Json.String message 
   ] res) in
-  
-  let! json = req_or (fail "Expected JSON POST") (Action.Convenience.get_json req) in
+
+  (* JSON is expected as : 
+     - entire json POST payload
+     - string inside "json" form/urlencoded POST field
+     - string inside "json" GET field
+  *)
+
+  let! json = req_or (fail "Expected JSON POST") begin match req # post with 
+    | Some (`JSON json) -> Some json 
+    | Some (`POST post) when BatPMap.mem "json" post -> 
+      (try Some (Json.unserialize (BatPMap.find "json" post)) with _ -> None)
+    | _ -> match req # get "json" with 
+      | None -> None
+      | Some str -> try Some (Json.unserialize str) with _ -> None
+  end in 
+
   let! beat = req_or (fail "Invalid JSON format") (ArgFmt.of_json_safe json) in
   let  aid, secret = req # args in
 
