9b63a5099dea7ed268819e9465b27495adabc6b9
Victor Nicollet <victor@nicollet.net>
2013-03-08 00:15:50 +0100

    C. Check account expiration date

--- ocaml/mAccount.ml
+++ ocaml/mAccount.ml
@@ -10,6 +10,7 @@ module Data = struct
       secret  "s" : string ;
       plan    "p" : [ `Trial "t" | `Basic "b" ] ;
       created "c" : float ;
+      paid    "m" : float ; 
     }
   end
   include T
@@ -22,6 +23,7 @@ type t = Data.t = {
   secret  : string ;
   plan    : [ `Trial | `Basic ] ;
   created : float ;
+  paid    : float ; 
 }
 
 let get aid = 
@@ -34,7 +36,32 @@ let create () =
     |> Digest.string 
     |> Digest.to_hex 
   in
-  let  account = { secret ; plan = `Trial ; created } in
+  let  account = { secret ; plan = `Trial ; created ; paid = 0.0 } in
   let! aid = ohm (Tbl.create account) in
   return (account, aid) 
   
+let one_month_later time = 
+  let t = Unix.localtime time in
+  let t' = Unix.({ t with tm_mon = t.tm_mon + 1 }) in
+  fst (Unix.mktime t')
+
+let expires account = 
+  match account.Data.plan with 
+    | `Trial -> one_month_later account.Data.created
+    | `Basic -> one_month_later account.Data.paid
+
+let shortest_month = 28. *. 24. *. 3600.
+let longest_month = 31. *. 24. *. 3600.
+
+let expired account now = 
+  (* Avoid complex calls to unix localtime functions by 
+     using a heuristic that only causes localtime to be
+     computed when an account has expired between 28 and 
+     31 days ago. 
+  *)
+  let last = match account.Data.plan with 
+    | `Trial -> account.Data.created
+    | `Basic -> account.Data.paid
+  in
+  (last +. longest_month <= now) 
+  && ((last +. shortest_month > now) || (expires account > now))
--- ocaml/mAccount.mli
+++ ocaml/mAccount.mli
@@ -4,9 +4,12 @@ type t = {
   secret  : string ;
   plan    : [ `Trial | `Basic ] ;
   created : float ;
+  paid    : float ; 
 }
 
 val create : unit -> (#O.ctx, t * IAccount.t) Ohm.Run.t
 
 val get : IAccount.t -> (#O.ctx, t option) Ohm.Run.t
 
+val expired : t -> float -> bool
+val expires : t -> float
