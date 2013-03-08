94887143feca383e65135b4e6091edc21a54243a
Victor Nicollet <victor@nicollet.net>
2013-03-08 02:13:59 +0100

    D. Finish writing intro page

--- assets/account/def.adlib.ml
+++ assets/account/def.adlib.ml
@@ -1,2 +1,8 @@
 | `Account_Expires of (float * float) 
 | `Account_HereIsYourApiUrl
+| `Account_ExampleUsage 
+| `Account_ExampleTest
+| `Account_ExampleComment 
+| `Account_ExampleComment2 
+| `Account_ExampleCrontab 
+| `Account_RefreshWhenReady 
--- assets/account/en.adlib.ml
+++ assets/account/en.adlib.ml
@@ -10,3 +10,9 @@
     !! "This account will expire %s." duration 
 end
 | `Account_HereIsYourApiUrl -> "Hello ! Here is your very own private API URL:"
+| `Account_ExampleUsage ->"You need to POST a heartbeat in JSON format to this URL."
+| `Account_ExampleComment -> "Send a 'this server is alive' heartbeat"
+| `Account_ExampleComment2 -> "Panic if 13 minutes pass with no heartbeat"
+| `Account_ExampleTest -> "Always test the heartbeat script manually at least once."
+| `Account_ExampleCrontab -> "Then, make sure that the heartbeat is sent at regular intervals, such as every five minutes." 
+| `Account_RefreshWhenReady -> "When you're done, refresh this page to see your server status."
